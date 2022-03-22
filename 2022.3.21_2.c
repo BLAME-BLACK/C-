50eea1195171f161ff4d54948514e7a82649d3a2
BLAME-BLACK <76458384+BLAME-BLACK@users.noreply.github.com>
2022-03-22 08:27:15 +0800

    Add files via upload

---
+++ 2022.3.21_2.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARINGS 1
+//2.¼ÆËã1£¡+2£¡+3£¡......+n£¡
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int n = 0;
+	int sum = 0;
+	int ret = 1;
+	scanf_s("%d", &n);
+	for(i=1;i<=n;i++)
+	{
+		ret *= i;
+		sum += ret;
+	}
+	printf("%d\n", sum);
+	return 0;
+}
