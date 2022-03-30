e80ae61d7838a7be42e5521c479b2e4646037e41
BLAME-BLACK <76458384+BLAME-BLACK@users.noreply.github.com>
2022-03-30 08:59:28 +0800

    Add files via upload

---
+++ 2022.3.25_5.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARINGS 1
+//5.打印100-200之间的素数
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
+	int i = 0;
+	int count = 0;
+	for (i = 101;i <=200;i+=2)
+	{
+		int j = 0;
+		for (j = 2;j <=sqrt(i);j++)
+		{
+			if (0 == i % j)
+			{
+				break;
+			}
+		}
+			if (j >sqrt(i))
+			{
+				count++;
+				printf("%d ", i);
+			}
+	}
+	printf("\ncount:%d ", count);
+	return 0;
+}
