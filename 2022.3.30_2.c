6ce4c7e8fd5077963fc6c701b1d3950aef57e23b
BLAME-BLACK <76458384+BLAME-BLACK@users.noreply.github.com>
2022-04-01 08:10:42 +0800

    Add files via upload

---
+++ 2022.3.30_2.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARINGS 1
+//2.计算1/1-1/2+1/3-1/4+1-5.....+1/99-1/100的结果，并打印
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	double sum = 0.0;
+	int flag = 1;
+	for (i = 1;i <= 100;i++)
+	{
+		sum += flag*1.0 / i;
+		flag = -flag;
+	}
+	printf("sum:%lf\n", sum);
+	return 0;
+}
