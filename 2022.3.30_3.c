2713fa7708d26f37e544450c886fe72adae44821
BLAME-BLACK <76458384+BLAME-BLACK@users.noreply.github.com>
2022-04-02 07:56:30 +0800

    Add files via upload

---
+++ 2022.3.30_3.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARINGS 1
+//3.求十个整数的最大值
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,7,6,9,8,10 };
+	int i = 0;
+	int max = arr[0];
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 1;i < sz;i++)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+	printf("max=%d\n", max);
+	return 0;
+}
