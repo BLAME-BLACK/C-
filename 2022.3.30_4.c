16a18f8ae1ccd09b9afe3a2d3766354aac79ddcf
BLAME-BLACK <76458384+BLAME-BLACK@users.noreply.github.com>
2022-04-03 07:48:19 +0800

    Add files via upload

---
+++ 2022.3.30_4.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARINGS 1
+//4.´òÓ¡9*9¿Ú¾÷±í
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 1;i <= 9;i++)
+	{
+		int j = 0;
+		for (j = 1;j <=i;j++)
+		{
+			printf("%d*%d=%-2d ",i,j,i*j );
+		}
+		printf("\n");
+	}
+	return 0;
+}
