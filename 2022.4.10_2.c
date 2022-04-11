34cf34112544fc7dd7fea597aa0ba123c28805a7
BLAME-BLACK <76458384+BLAME-BLACK@users.noreply.github.com>
2022-04-11 12:46:25 +0800

    Add files via upload

---
+++ 2022.4.10_2.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARINGS 1
+//¼ÆËãnµÄ½×³Ë
+#include<stdio.h>
+int Fac(int n)
+{
+	if (1 <= n)
+	{
+		return 1;
+	}
+	else
+	{
+		return n*Fac(n - 1);
+	}
+}
+int main()
+{
+	int n = 0;
+	int ret;
+	scanf("%d", &n);
+	ret = Fac(n);
+	printf("%d\n", ret);
+	return 0;
+}
